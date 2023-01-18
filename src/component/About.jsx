fd6607efc5bdd502acf289498b29f2f7bb308a84
95SR <greensketch26@gmail.com>
2023-01-19 00:00:45 +0900

    edit aboutme

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -6,7 +6,26 @@ function About() {
     <div className='about'>
       <div className="container">
         <div className="title">
-          About
+          <div className="number">
+          01/<span>03</span> 
+          </div>
+
+          <div className="subtitles">
+            <div className="sub">
+              Intro
+            </div>
+            <div className="sub">
+              Background
+            </div>
+            <div className="sub">
+              Skill
+            </div>
+          </div>
+
+          <div className="scroll">
+            <span>  </span> scroll
+          </div>
+        
         </div>
         <div className="content">
           content
