0823cd61d1d6bd4b468a5b0c7634f8725e5f5c55
riri13 <greensketch26@gmail.com>
2023-01-19 17:59:34 +0900

    update about page

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,38 +1,99 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './About.css'
 
 function About() {
+  const [scrollY, setScrollY] = useState(0);
+const [pageNum, setPageNum] = useState('01')
+  
+    const handleScroll = e => {
+      let scroll=e.target.scrollTop/2;
+      setScrollY(scroll)
+      console.log(scrollY)
+      if(scrollY < 70) {
+        setPageNum('01')
+      } else if(scrollY >= 70 && scrollY < 200){
+        setPageNum('02')
+      } else {setPageNum('03')}
+    }
+
+
+
+    const [mousePos, setMousePos] = useState({});
+
+    useEffect(() => {
+      const handleMouseMove = (event) => {
+        setMousePos({ x: event.clientX, y: event.clientY });
+      };
+  
+      window.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        window.removeEventListener(
+          'mousemove',
+          handleMouseMove
+        );
+      };
+    }, []);
+
+    
   return (
-    <div className='about'>
-      <div className="container">
-        <div className="title">
+    <div className='about'  >
+      
+      <div className="container"  onScroll={handleScroll} >
+      <div className="cursor-custom" style={{transform: `translate3d(${mousePos.x-80}px, ${mousePos.y-90}px, 0)`}}>
+      
+      {
+        scrollY < 260 
+        ? 
+
+        <div className="scroll">
+            <span>  </span> scroll
+      </div>
+      :
+
+      <div className="scroll up">
+            <span>  </span> scroll up
+      </div>
+
+      }
+      
+        </div>
+      <div className="blank"></div>
+        <div className="title" >
           <div className="number">
-          01/<span>03</span> 
+          {pageNum}/<span>03</span> 
           </div>
 
-          <div className="subtitles">
-            <div className="sub">
+          <div className="subtitles" >
+            <div className="sub intro" style={{transform: `translateY(${-scrollY}px)`}}>
               Intro
             </div>
-            <div className="sub">
+            <div className="sub bg" style={{transform: `translateY(${-scrollY}px)`}}>
               Background
             </div>
-            <div className="sub">
+            <div className="sub skill" style={{transform: `translateY(${-scrollY}px)`}}>
               Skill
             </div>
           </div>
 
-          <div className="scroll">
-            <span>  </span> scroll
-          </div>
+       
+
+          
+          
         
         </div>
+
         <div className="content">
-          content
+          {pageNum == '01' ? <div>p</div> : <div>s</div>}
+        
+            
         </div>
+
+        
+
       </div>
 
-      
+     
       
     </div>
   )
